8a52801cc02ad339e3928c9042058eff83bc6163
PARK EUN HEE <mii0329@naver.com>
2023-05-06 18:24:21 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// 1. 왼쪽엔 연락처 등록하는 폼, 오른쪽에는 연락처 리스트와 search창
+// 2. 리스트에 유저 이름과 연락처를 추가 할 수 있다.
+// 3. 리스트에 아이템이 몇개 있는지 보임.
+// 4. 사용자가 유저를 이름검색으로 찾을 수 있다.
+
+function App() {
+  return (
+    <div>
+      <h1 className="title">PHONE BOOK</h1>
+    </div>
+  );
+}
+
+export default App;
